9d67ce6f69bb2d75bc44203104eebf8b9d12f0fb
Juho Jeong <aspectran@gmail.com>
2020-01-22 21:07:15 +0900

    Updated

--- setup/install-app.sh
+++ setup/install-app.sh
@@ -4,11 +4,10 @@
 
 echo "Installing application to $BASE_DIR ..."
 
-if [ ! -d "$BUILD_DIR" ]; then
-  mkdir "$BUILD_DIR"
+if [ ! -d "$REPO_DIR" ]; then
+  [ ! -d "$BUILD_DIR" ] && mkdir "$BUILD_DIR"
   cd "$BUILD_DIR" || exit
   git clone "$REPO_URL" "$APP_NAME"
-  cd "$REPO_DIR" || exit
 fi
 
 cp "$REPO_DIR/setup/app.conf" "$BASE_DIR" || exit
