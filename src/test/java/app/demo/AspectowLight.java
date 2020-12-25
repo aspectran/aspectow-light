6e33a20ed1730cc9102a2cfede9946f761dd84d9
Juho Jeong <aspectran@gmail.com>
2020-12-26 02:28:36 +0900

    Update

--- src/test/java/app/demo/AspectowLight.java
+++ src/test/java/app/demo/AspectowLight.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package app;
+package app.demo;
 
 import com.aspectran.core.util.ResourceUtils;
 import com.aspectran.shell.AspectranShell;
