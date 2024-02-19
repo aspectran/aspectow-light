2cdda9c8e3a3e85c98ca02ecb49c2578219f991e
Juho Jeong <aspectran@gmail.com>
2024-02-19 17:27:39 +0900

    Update

--- src/main/java/aspectow/demo/examples/SampleResource.java
+++ src/main/java/aspectow/demo/examples/SampleResource.java
@@ -20,6 +20,7 @@ import com.aspectran.utils.ResourceUtils;
 import com.aspectran.utils.annotation.jsr305.NonNull;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class SampleResource {
 
