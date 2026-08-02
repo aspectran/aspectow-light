cdd0c8ee0e54e2ef7a475e200bc428451dbf4123
Juho Jeong <aspectran@gmail.com>
2026-08-02 12:34:44 +0900

    refactor(scripts): streamline argument passing in `daemon.sh` script

--- setup/scripts/linux/daemon.sh
+++ setup/scripts/linux/daemon.sh
@@ -6,4 +6,7 @@ set -e
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 . "$SCRIPT_DIR/app.conf"
 
-"$DEPLOY_DIR/bin/jsvc-daemon.sh" --proc-name "$PROC_NAME" --pid-file "$PID_FILE" --user "$DAEMON_USER" "$1"
+"$DEPLOY_DIR/bin/jsvc-daemon.sh" \
+  --proc-name "$PROC_NAME" \
+  --user "$DAEMON_USER" \
+  "$@"
